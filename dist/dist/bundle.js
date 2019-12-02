ee03b716c58eeac2f02671308d9996c7b016cdcc
ErikElliottCreates <erikelliott92@gmail.com>
2019-12-02 10:34:14 -0800

    fix map

--- dist/dist/bundle.js
+++ dist/dist/bundle.js
@@ -280,7 +280,7 @@ var cityParksGraph = function cityParksGraph() {
   d3.json("https://d3js.org/us-10m.v1.json", function (error, us) {
     if (error) throw error; // border
 
-    svg.append("g").attr("class", "states").selectAll("path").data(topojson.feature(us, us.objects.states).features).enter().append("path").style("fill", "rgb(28,g 66, 19)").attr("d", path); // counties
+    svg.append("g").attr("class", "states").selectAll("path").data(topojson.feature(us, us.objects.states).features).enter().append("path").style("fill", "rgb(28, 66, 19)").attr("d", path); // counties
 
     svg.append('path').attr('class', 'county-borders').attr('d', path(topojson.mesh(us, us.objects.counties, function (a, b) {
       return a !== b && !(a.id / 1000 ^ b.id / 1000);
